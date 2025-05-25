77343e5d5eb4473c5131ecc28ce25c22b7f026cc
frappe <frappe@KEITH-PC>
2025-05-25 14:03:46 +0300

    feat: Initialize App

---
+++ README.md
@@ -0,0 +1,33 @@
+### SEAL HRMS
+
+SEAL Customizations for Frappe HRMS
+
+### Installation
+
+You can install this app using the [bench](https://github.com/frappe/bench) CLI:
+
+```bash
+cd $PATH_TO_YOUR_BENCH
+bench get-app $URL_OF_THIS_REPO --branch develop
+bench install-app seal_hrms
+```
+
+### Contributing
+
+This app uses `pre-commit` for code formatting and linting. Please [install pre-commit](https://pre-commit.com/#installation) and enable it for this repository:
+
+```bash
+cd apps/seal_hrms
+pre-commit install
+```
+
+Pre-commit is configured to use the following tools for checking and formatting your code:
+
+- ruff
+- eslint
+- prettier
+- pyupgrade
+
+### License
+
+gpl-3.0
---
+++ seal_hrms/hooks.py
@@ -0,0 +1,244 @@
+app_name = "seal_hrms"
+app_title = "SEAL HRMS"
+app_publisher = "Stelden EA Ltd"
+app_description = "SEAL Customizations for Frappe HRMS"
+app_email = "dev@stelden.com"
+app_license = "gpl-3.0"
+
+# Apps
+# ------------------
+
+# required_apps = []
+
+# Each item in the list will be shown as an app in the apps page
+# add_to_apps_screen = [
+# 	{
+# 		"name": "seal_hrms",
+# 		"logo": "/assets/seal_hrms/logo.png",
+# 		"title": "SEAL HRMS",
+# 		"route": "/seal_hrms",
+# 		"has_permission": "seal_hrms.api.permission.has_app_permission"
+# 	}
+# ]
+
+# Includes in <head>
+# ------------------
+
+# include js, css files in header of desk.html
+# app_include_css = "/assets/seal_hrms/css/seal_hrms.css"
+# app_include_js = "/assets/seal_hrms/js/seal_hrms.js"
+
+# include js, css files in header of web template
+# web_include_css = "/assets/seal_hrms/css/seal_hrms.css"
+# web_include_js = "/assets/seal_hrms/js/seal_hrms.js"
+
+# include custom scss in every website theme (without file extension ".scss")
+# website_theme_scss = "seal_hrms/public/scss/website"
+
+# include js, css files in header of web form
+# webform_include_js = {"doctype": "public/js/doctype.js"}
+# webform_include_css = {"doctype": "public/css/doctype.css"}
+
+# include js in page
+# page_js = {"page" : "public/js/file.js"}
+
+# include js in doctype views
+# doctype_js = {"doctype" : "public/js/doctype.js"}
+# doctype_list_js = {"doctype" : "public/js/doctype_list.js"}
+# doctype_tree_js = {"doctype" : "public/js/doctype_tree.js"}
+# doctype_calendar_js = {"doctype" : "public/js/doctype_calendar.js"}
+
+# Svg Icons
+# ------------------
+# include app icons in desk
+# app_include_icons = "seal_hrms/public/icons.svg"
+
+# Home Pages
+# ----------
+
+# application home page (will override Website Settings)
+# home_page = "login"
+
+# website user home page (by Role)
+# role_home_page = {
+# 	"Role": "home_page"
+# }
+
+# Generators
+# ----------
+
+# automatically create page for each record of this doctype
+# website_generators = ["Web Page"]
+
+# Jinja
+# ----------
+
+# add methods and filters to jinja environment
+# jinja = {
+# 	"methods": "seal_hrms.utils.jinja_methods",
+# 	"filters": "seal_hrms.utils.jinja_filters"
+# }
+
+# Installation
+# ------------
+
+# before_install = "seal_hrms.install.before_install"
+# after_install = "seal_hrms.install.after_install"
+
+# Uninstallation
+# ------------
+
+# before_uninstall = "seal_hrms.uninstall.before_uninstall"
+# after_uninstall = "seal_hrms.uninstall.after_uninstall"
+
+# Integration Setup
+# ------------------
+# To set up dependencies/integrations with other apps
+# Name of the app being installed is passed as an argument
+
+# before_app_install = "seal_hrms.utils.before_app_install"
+# after_app_install = "seal_hrms.utils.after_app_install"
+
+# Integration Cleanup
+# -------------------
+# To clean up dependencies/integrations with other apps
+# Name of the app being uninstalled is passed as an argument
+
+# before_app_uninstall = "seal_hrms.utils.before_app_uninstall"
+# after_app_uninstall = "seal_hrms.utils.after_app_uninstall"
+
+# Desk Notifications
+# ------------------
+# See frappe.core.notifications.get_notification_config
+
+# notification_config = "seal_hrms.notifications.get_notification_config"
+
+# Permissions
+# -----------
+# Permissions evaluated in scripted ways
+
+# permission_query_conditions = {
+# 	"Event": "frappe.desk.doctype.event.event.get_permission_query_conditions",
+# }
+#
+# has_permission = {
+# 	"Event": "frappe.desk.doctype.event.event.has_permission",
+# }
+
+# DocType Class
+# ---------------
+# Override standard doctype classes
+
+# override_doctype_class = {
+# 	"ToDo": "custom_app.overrides.CustomToDo"
+# }
+
+# Document Events
+# ---------------
+# Hook on document methods and events
+
+# doc_events = {
+# 	"*": {
+# 		"on_update": "method",
+# 		"on_cancel": "method",
+# 		"on_trash": "method"
+# 	}
+# }
+
+# Scheduled Tasks
+# ---------------
+
+# scheduler_events = {
+# 	"all": [
+# 		"seal_hrms.tasks.all"
+# 	],
+# 	"daily": [
+# 		"seal_hrms.tasks.daily"
+# 	],
+# 	"hourly": [
+# 		"seal_hrms.tasks.hourly"
+# 	],
+# 	"weekly": [
+# 		"seal_hrms.tasks.weekly"
+# 	],
+# 	"monthly": [
+# 		"seal_hrms.tasks.monthly"
+# 	],
+# }
+
+# Testing
+# -------
+
+# before_tests = "seal_hrms.install.before_tests"
+
+# Overriding Methods
+# ------------------------------
+#
+# override_whitelisted_methods = {
+# 	"frappe.desk.doctype.event.event.get_events": "seal_hrms.event.get_events"
+# }
+#
+# each overriding function accepts a `data` argument;
+# generated from the base implementation of the doctype dashboard,
+# along with any modifications made in other Frappe apps
+# override_doctype_dashboards = {
+# 	"Task": "seal_hrms.task.get_dashboard_data"
+# }
+
+# exempt linked doctypes from being automatically cancelled
+#
+# auto_cancel_exempted_doctypes = ["Auto Repeat"]
+
+# Ignore links to specified DocTypes when deleting documents
+# -----------------------------------------------------------
+
+# ignore_links_on_delete = ["Communication", "ToDo"]
+
+# Request Events
+# ----------------
+# before_request = ["seal_hrms.utils.before_request"]
+# after_request = ["seal_hrms.utils.after_request"]
+
+# Job Events
+# ----------
+# before_job = ["seal_hrms.utils.before_job"]
+# after_job = ["seal_hrms.utils.after_job"]
+
+# User Data Protection
+# --------------------
+
+# user_data_fields = [
+# 	{
+# 		"doctype": "{doctype_1}",
+# 		"filter_by": "{filter_by}",
+# 		"redact_fields": ["{field_1}", "{field_2}"],
+# 		"partial": 1,
+# 	},
+# 	{
+# 		"doctype": "{doctype_2}",
+# 		"filter_by": "{filter_by}",
+# 		"partial": 1,
+# 	},
+# 	{
+# 		"doctype": "{doctype_3}",
+# 		"strict": False,
+# 	},
+# 	{
+# 		"doctype": "{doctype_4}"
+# 	}
+# ]
+
+# Authentication and authorization
+# --------------------------------
+
+# auth_hooks = [
+# 	"seal_hrms.auth.validate"
+# ]
+
+# Automatically update python controller files with type annotations for this app.
+# export_python_type_annotations = True
+
+# default_log_clearing_doctypes = {
+# 	"Logging DocType Name": 30  # days to retain logs
+# }
+
