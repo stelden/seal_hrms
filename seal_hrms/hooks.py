8fc3dac9c1396189758b4cd60dfcd2e0d3271cce
Dennis Keith <45001106+dmbabu@users.noreply.github.com>
2025-09-18 12:29:41 +0300

    Update hooks.py

--- seal_hrms/hooks.py
+++ seal_hrms/hooks.py
@@ -10,9 +10,7 @@ app_license = "gpl-3.0"
 
 required_apps = [
     "frappe/erpnext", 
-    "hrms", 
-    "https://github.com/stelden/seal_common",
-    "https://github.com/stelden/seal_accounts"
+    "hrms"
 ]
 
 # Each item in the list will be shown as an app in the apps page
