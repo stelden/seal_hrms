d2547b2006ef73518496f0d403cf7fed20f7c0f1
Dennis <dev@stelden.com>
2025-09-25 02:52:34 +0300

    Removed required apps

--- seal_hrms/hooks.py
+++ seal_hrms/hooks.py
@@ -8,12 +8,12 @@ app_license = "gpl-3.0"
 # Apps
 # ------------------
 
-required_apps = [
-    "frappe/erpnext", 
-    "hrms", 
-    "https://github.com/stelden/seal_common",
-    "https://github.com/stelden/seal_accounts"
-]
+# required_apps = [
+#     "frappe/erpnext", 
+#     "hrms", 
+#     "https://github.com/stelden/seal_common",
+#     "https://github.com/stelden/seal_accounts"
+# ]
 
 # Each item in the list will be shown as an app in the apps page
 # add_to_apps_screen = [
